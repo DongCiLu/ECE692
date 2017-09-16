f8479022e6096c06c77b70ac2fce228f3e46c537
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-09-16 16:18:25 -0400

    update setup

--- Project2/setup.sh
+++ Project2/setup.sh
@@ -1,2 +1,4 @@
-wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+scp laterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/cifar-10-python.tar.gz .
 tar xzvf cifar-10-python.tar.gz
+scp laterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/tensorflow-resnet-pretrained-20160509.tar.gz .
+tar xzvf tensorflow-resnet-pretrained-20160509.tar.gz
