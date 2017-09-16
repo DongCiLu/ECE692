72922aad06aa13a7782660aa6d8ebc56d0261550
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-09-16 16:22:46 -0400

    fix

--- Project2/setup.sh
+++ Project2/setup.sh
@@ -1,4 +1,4 @@
-scp laterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/cifar-10-python.tar.gz .
+scp lanterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/cifar-10-python.tar.gz .
 tar xzvf cifar-10-python.tar.gz
-scp laterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/tensorflow-resnet-pretrained-20160509.tar.gz .
+scp lanterns2.eecs.utk.edu:/local_scratch/Datasets/deeplearning_data/tensorflow-resnet-pretrained-20160509.tar.gz .
 tar xzvf tensorflow-resnet-pretrained-20160509.tar.gz
