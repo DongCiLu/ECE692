ae3236de213eaccc32d75419c5a719a58681a9a3
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-09-18 16:53:26 -0400

    change to slim

--- Project2/setup_lenet.py
+++ Project2/setup_lenet.py
@@ -2,4 +2,4 @@ git clone https://github.com/tensorflow/models.git
 
 DATA_DIR=./cifar10
 python models/slim/download_and_convert_data.py --dataset_name=cifar10 --dataset_dir="${DATA_DIR}"
-cp -r models/slim/datasets slimdatasets
+cp -r models/slim/datasets .
