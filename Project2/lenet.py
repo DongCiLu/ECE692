989897ff7f8a06fa9bcd7ee4e1a841f542797299
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-09-18 17:40:36 -0400

    add slim train loop

--- Project2/lenet.py
+++ Project2/lenet.py
@@ -4,99 +4,87 @@ import numpy as np
 
 slim = tf.contrib.slim
 
-class Lenet(object):
-    def __init__(self, log_dirname, lr):
-        self.log_dirname = log_dirname
-        self.lr = lr
-
-        self.kernal = [5, 5]
-        self.feature = [6, 16]
-        self.pool = [2, 2]
-        self.fc = [1024]
-
-    def network(self, images, n_class=10, \
-            is_training=False, scope='LeNet'):
-        end_points = {}
-        with tf.variable_scope(scope, 'LeNet', [images, n_class]):
-            # first convolution layer
-            self.net = slim.conv2d(image, self.feature[0], \
-                    [self.kernal[0], self.kernal[0]], scope='conv1')
-            self.net = slim.max_pool2d(self.net, \
-                    [self.pool[0], self.pool[0]], scope='pool1')
-            # second convolution layer
-            self.net = slim.conv2d(self.net, self.feature[1], \
-                    [self.kernal[1], self.kernal[1]], scope='conv2')
-            self.net = slim.max_pool2d(self.net, \
-                    [self.pool[1], self.pool[1]], scope='pool2')
-            # flatten output from convolutional layer
-            self.net = slim.flatten(self.net)
-            self.end_points['Flatten'] = self.net
-            # first fully connected layer
-            self.net = slim.fully_connected(self.net, \
-                    self.fc[0], scope='fc3')
-            # dropout regulation for fully connected layer
-            dropuot_keep_prob = 0.5
-            self.net = slim.dropout(net, dropout_keep_prob, \
-                    is_training=is_training, scope='dropout3')
-            # output fully connected layer
-            logits = slim.fully_connected(self.net, n_class, \
-                    activation_fn=None, scope='fc4')
-
-            end_points['Logits'] = self.logits 
-            predictions = tf.argmax(logits, 1)
-            end_points['Predictions'] = predictions
-
-            return predictions, end_points
-
-    def train(self, x_train, y_train):
-        self.sess = tf.Session()
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
-        self.eval() # creating evaluation
-        train_size = x_train.shape[0]
-        for i in range(self.epochs):
-            start = (i * self.batch_size) % train_size
-            if start + self.batch_size > train_size:
-                continue
-            x_batch = x_train[start: start + self.batch_size]
-            y_batch = y_train[start: start + self.batch_size]
-            if i % 100 == 0:
-                train_acc = self.sess.run(self.accuracy, \
-                        feed_dict={self.x: x_batch, self.y_: y_batch, \
-                        self.keep_prob: 1.0})
-                print('step %d, training accuracy %g' % (i, train_acc))
-            self.sess.run([self.train_step], \
-                    feed_dict={self.x: x_batch, self.y_: y_batch, \
-                    self.keep_prob: 0.5})
-        
-    def eval(self):
-        correct_prediction = tf.equal(\
-                tf.argmax(self.y_conv, 1), tf.argmax(self.y_, 1))
-        self.accuracy = tf.reduce_mean(\
-                tf.cast(correct_prediction, tf.float32))
-
-    def test_eval(self, x_test, y_test):
-        self.eval()
-        test_acc = self.sess.run(self.accuracy, feed_dict={
-                self.x: x_test, self.y_: y_test, self.keep_prob: 1.0})
-        print('test accuracy %g' % test_acc)
-
-if __name__ == '__main__':
-    log_dirname = 'tensorflow_log_lenet/'
-    data_dirname = './datasets'
-    # load training data
-    dataset = cifar10.get_split('train', data_dirname)
+def load_batch(dataset, batch_size):
     provider = slim.dataset_data_provider.DatasetDataProvider(dataset)
     [image, label] = provider.get(['image', 'label'])
+    images, labels = tf.train.batch(\
+            [image, label], \
+            batch_size = batch_size, \
+            capacity = 2 * batch_size)
+    return images, labels
 
-    '''
-    lr = 1e-3
+def leNet(images, n_class=10, \
+        is_training=False, scope='LeNet'):
+
+    kernal = [5, 5]
+    feature = [6, 16]
+    pool = [2, 2]
+    fc = [1024]
+
+    end_points = {}
+    with tf.variable_scope(scope, 'LeNet', [images, n_class]):
+        # first convolution layer
+        net = slim.conv2d(image, feature[0], \
+                [kernal[0], kernal[0]], scope='conv1')
+        net = slim.max_pool2d(net, \
+                [pool[0], pool[0]], scope='pool1')
+        # second convolution layer
+        net = slim.conv2d(net, feature[1], \
+                [kernal[1], kernal[1]], scope='conv2')
+        net = slim.max_pool2d(net, \
+                [pool[1], pool[1]], scope='pool2')
+        # flatten output from convolutional layer
+        net = slim.flatten(net)
+        end_points['Flatten'] = net
+        # first fully connected layer
+        net = slim.fully_connected(net, \
+                fc[0], scope='fc3')
+        # dropout regulation for fully connected layer
+        dropuot_keep_prob = 0.5
+        net = slim.dropout(net, dropout_keep_prob, \
+                is_training=is_training, scope='dropout3')
+        # output fully connected layer
+        logits = slim.fully_connected(net, n_class, \
+                activation_fn=None, scope='fc4')
+
+        end_points['Logits'] = logits 
+        predictions = tf.argmax(logits, 1)
+        end_points['Predictions'] = predictions
+
+        return logits, end_points
+
+if __name__ == '__main__':
+    train_dir = './lenet_log/'
+    data_dirname = './datasets'
+    lr = 0.01
     epochs = 1000
     batch_size = 128
-    input_size = [32, 32, 3]
-    n_class = 10
 
-    cnn = CNN(log_dirname, lr, epochs, batch_size, input_size, n_class)
-    cnn.train(x_train, y_train)
-    cnn.test_eval(x_test, y_test)
-    '''
+    # load training data
+    dataset = cifar10.get_split('train', data_dirname)
+    images, labels = load_batch(dataset, batch_size = batch_size)
+
+    # define the loss
+    logits, end_points = leNet(images, \
+            n_class = dataset.num_classes, \
+            is_training = True)
+    one_hot_labels = slim.one_hot_encoding(labels, dataset.num_classes)
+    slim.losses.sofrmax_cross_entropy(logits, one_hot_labels)
+    total_loss = slim.losses.get_total_loss()
+
+    # monitor the total loss
+    tf.summary.scalar('losses/Total Loss', total_loss)
+
+    # specify the optimizer and create the train op
+    optimizer = tf.train.AdamOptimizer(learning_rate = lr)
+    train_op = slim.learning.create_train_op(total_loss, optimizer)
+
+    # run the training
+    final_loss = slim.learning.train( \
+            train_op, \
+            logdir = train_dir, \
+            num_of_steps = epochs, \
+            save_summaries_secs = 1)
+
+    print 'Finished_traing. Final batch loss {}'.format(final_loss)
+
