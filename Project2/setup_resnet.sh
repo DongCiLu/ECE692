5658889fe96fb44d4f6fe0e1bf0f1db4a9fa5d0a
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-09-18 09:58:16 -0400

    finalize resnet

--- Project2/setup_resnet.sh
+++ Project2/setup_resnet.sh
@@ -1,16 +1,20 @@
-sudo apt-get install openjdk-8-jdk
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jdk
 sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update && sudo apt-get install oracle-java8-installer
+sudo apt-get update && sudo apt-get -y install oracle-java8-installer
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt-get -y install curl
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install bazel
+sudo apt-get update && sudo apt-get -y install bazel
 
 git clone https://github.com/tensorflow/models.git
 cd models
 
 curl -o cifar-10-binary.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz
-tar xzvg cifar-10-binary.tar.gz
-mv cifar-10-binary cifar-10
+tar xzvf cifar-10-binary.tar.gz
+mv cifar-10-batches-bin cifar10
 
 bazel build -c opt --config=cuda resnet/...
 
@@ -18,11 +22,11 @@ bazel-bin/resnet/resnet_main --train_data_path=cifar10/data_batch* \
                                --log_root=/tmp/resnet_model \
                                --train_dir=/tmp/resnet_model/train \
                                --dataset='cifar10' \
-                               --num_gpus=2
+                               --num_gpus=1
 
 # bazel-bin/resnet/resnet_main --eval_data_path=cifar10/test_batch.bin \
 #                               --log_root=/tmp/resnet_model \
 #                               --eval_dir=/tmp/resnet_model/test \
 #                               --mode=eval \
 #                               --dataset='cifar10' \
-#                               --num_gpus=2
+#                               --num_gpus=1
